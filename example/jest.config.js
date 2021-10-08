bd4d48f03a88cf64621011253f74b9b56eb89465
Robby Abaya <robby@getpinwheel.com>
2021-10-08 13:27:22 -0400

    Update jest config

---
+++ example/jest.config.js
@@ -0,0 +1,22 @@
+const {defaults: tsjPreset} = require('ts-jest/presets');
+
+module.exports = {
+  ...tsjPreset,
+  preset: 'react-native',
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1'
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)',
+  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '\\.snap$'],
+  cacheDirectory: '.jest/cache',
+  testEnvironment: 'jsdom',
+};
