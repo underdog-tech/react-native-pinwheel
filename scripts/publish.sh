5b3efeb578fea3ce56af6acb7da7fcbb94be4616
roscioli <octavio@getpinwheel.com>
2022-01-12 12:48:59 -0500

    add publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -17,13 +17,3 @@ npm publish
 git tag $pkgjsonversion
 
 git push --tags
-
-# - Create a new branch `git checkout -b release-2.3.4`
-# - Bump the version number in `package.json`: `"version": "2.3.4"`
-# - Add and commit to git `git add . && git commit -m 'Release 2.3.4'`
-# - `npm run build`
-# - `git tag '2.3.4'`
-# - `git push --tags`
-# - `git push origin release-2.3.4`
-# - `npm publish` (you will need to have set up your NPM credentials for this to work)
-# - Open a PR to merge the release branch back to master
