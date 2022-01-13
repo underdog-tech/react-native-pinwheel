ea495f71ddc2219a980cc3fefab674d904ec9abc
roscioli <octavio@getpinwheel.com>
2022-01-13 12:07:02 -0500

    check actual version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,10 +3,19 @@
 set -e
 
 pkgjsonversion=$(cat package.json | grep '"version":' | grep -E '([0-9]+\.[0-9]+\.[0-9]+[^"]*)' -o)
-latestversion=$(npm list --silent | head -n 1 | cut -d ' ' -f1 | cut -d '@' -f3)
+echo Got package.json version $pkgjsonversion
+
+latestversion=$(npm show @pinwheel/react-native-pinwheel |
+  head -n 2 |
+  tail -n 1 |
+  cut -d ' ' -f1 |
+  cut -d '@' -f3 |
+  sed $'s,\x1b\\[[0-9;]*[a-zA-Z],,g')
+echo Got last published version $latestversion
+echo
 
 if [ $pkgjsonversion == $latestversion ]; then
-  echo SKIPPING. Nothing to publish! No version change from $latestversion
+  echo SKIPPING. Nothing to publish, package.json version matches the latest.
   exit 0
 fi
 
