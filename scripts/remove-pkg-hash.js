f33e17ae2169667c21bf82d1405f99a2574360e7
roscioli <octavio@getpinwheel.com>
2022-11-28 16:08:22 -0500

    remove package hash

--- scripts/remove-pkg-hash.js
+++ scripts/remove-pkg-hash.js
@@ -9,7 +9,7 @@
 
 import fs from 'fs'
 
-const packageLockLocation = './example/package-lock.json'
+const packageLockLocation = '../example/package-lock.json'
 const pkgName = 'node_modules/@pinwheel/react-native-pinwheel'
 
 const contents = JSON.parse(fs.readFileSync(packageLockLocation, 'utf8'))
