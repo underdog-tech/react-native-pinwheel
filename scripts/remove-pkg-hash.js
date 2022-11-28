327b15cb9bfa7368fee9de10568368185db939de
roscioli <octavio@getpinwheel.com>
2022-11-28 16:20:53 -0500

    remove comments

--- scripts/remove-pkg-hash.js
+++ scripts/remove-pkg-hash.js
@@ -1,12 +1,3 @@
-
-    // "@pinwheel/react-native-pinwheel": {
-    //   "version": "file:../pinwheel-react-native-pinwheel-2.3.11.tgz",
-    //   "integrity": "sha512-uxHoWnRkjx9SDuA5EnaZSVtd2ELHMdTdWeme3WQzVv77VUzfP9vj+c2M3DDK4+UPRgg8K0PD+8BA9JvA6oTn9A==",
-    //   "requires": {
-    //     "hermes-engine": "^0.10.0"
-    //   }
-    // },
-
 import fs from 'fs'
 
 const packageLockLocation = '../example/package-lock.json'
