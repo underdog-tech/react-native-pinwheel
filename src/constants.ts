fa2775c788393817d0b604f8c28688343e09685d
roscioli <octavio@getpinwheel.com>
2022-11-29 08:26:17 -0500

    move to const file

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+/**
+ * Importing package.json here causes a problem with the folder structure when we npm pack and publish.
+ */
+export const VERSION = '2.3.12';
+
+export const LINK_PAGE_URL = 'https://cdn.getpinwheel.com/link-v2.3.0.html';
+
+export const PINWHEEL_DOMAIN = 'getpinwheel.com';
