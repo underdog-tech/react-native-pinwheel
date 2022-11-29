be398ced51d1e772afe718700fff351309856c5d
roscioli <octavio@getpinwheel.com>
2022-11-29 09:12:17 -0500

    update script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,13 +1,13 @@
 v_pkgjson=$(node -e "console.log(require('./package.json').version)")
-echo Found version in package.json: $v_pkgjson
+echo "---\n\nFound version in package.json: $v_pkgjson"
 
-echo \n\n Enter a new version number
+echo "\n\nEnter a new version number:"
 read v_new
 
-echo Updating package.json
-node -e "const fs=require('fs');const pkg=require('./package.json'); pkg.version=$v_new; fs.writeFileSync(JSON.stringify(pkg, null, 2), newContents)"
-echo Updated package.json
+echo ⌛ Updating package.json
+node -e "const fs=require('fs');const file='./package.json';const pkg=require(file); pkg.version='$v_new'; fs.writeFileSync(file, JSON.stringify(pkg, null, 2))"
+echo ✅ Updated package.json
 
-echo Updating src folder
+echo ⌛ Updating src folder
 node -e "const fs=require('fs');const file='./src/constants.ts';const contents=fs.readFileSync(file, 'utf8');const newContents = contents.replace(/VERSION\W*\=\W*[0-9\.]+/, 'VERSION = \'$v_new\''); fs.writeFileSync(file, newContents)"
-echo Updated src folder
+echo ✅ Updated src folder
