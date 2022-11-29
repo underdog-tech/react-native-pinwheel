376cfeabb1155e9896a42beb5a3b9c6b34110382
roscioli <octavio@getpinwheel.com>
2022-11-29 09:01:49 -0500

    src bump

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,3 +1,7 @@
-pkgjsonversion=$(node -e "console.log(require('./package.json').version)")
+v_pkgjson=$(node -e "console.log(require('./package.json').version)")
 
-echo Found version in package.json: $pkgjsonversion
+echo Found version in package.json: $v_pkgjson
+
+v_src=$(node -e "const fs=require('fs');const file='./src/constants.ts';const contents=fs.readFileSync(file, 'utf8');const newContents = contents.replace(/VERSION\W*\=\W*[0-9\.]+/, 'VERSION = \'$v_pkgjson\''); console.log(newContents)")
+
+echo Updated src folder
