bbdd57d4f2eb4c2e8501013232520f81d148b775
Rene Loperena <reneloperena@live.com>
2023-01-04 13:24:00 -0600

    Add version ranges for supported peer dependencies rather than fixed versions (#70)
    * Update peer dependencies to match newer versions
    
    * Update readme
    
    * More restrictive react version
    
    * Update example versions
    
    * add circle ci check
    
    * this test should fail ci
    
    * this test should pass ci
    
    * this test should fail ci
    
    * this test should pass ci
    
    Co-authored-by: roscioli <octavio@getpinwheel.com>

---
+++ scripts/check-version-matchups.cjs
@@ -0,0 +1,18 @@
+const baseVersion = require('../package.json').version
+const exampleVersion = require('../example/package.json').version
+const packageName = '@pinwheel/react-native-pinwheel'
+const installVersion = require('../example/package.json')
+  .dependencies[packageName]
+  .match(/[0-9]+\.[0-9]+\.[0-9]+\..*/)
+  [0]
+  .replace('.tgz', '')
+
+console.log(`Got package version (package.json): ${baseVersion}`)
+console.log(`Got example app (example/package.json) version: ${exampleVersion}`)
+console.log(`Got example app installation of package (${packageName}): ${installVersion}\n\n`)
+
+if (baseVersion === exampleVersion && exampleVersion === installVersion) {
+  console.log('success, check passed')
+} else {
+  throw new Error('Versions did not match up. Please sync them all.')
+}
