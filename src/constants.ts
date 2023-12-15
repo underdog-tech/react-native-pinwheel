75ec11a6ccd5a731e6fcad7cb64ce1875b8f433f
Rene Martin Loperena Garcia <rene@pinwheelapi.com>
2023-12-15 16:54:11 -0500

    Fix version

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 /**
  * Importing package.json here causes a problem with the folder structure when we npm pack and publish.
  */
-export const VERSION = '3.0.0';
+export const VERSION = '3.0.0-alpha.1';
 
 export const LINK_PAGE_URL = 'https://cdn.getpinwheel.com/link-v2.3.0.html';
 
