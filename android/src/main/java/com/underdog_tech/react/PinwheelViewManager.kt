fe046f89233a63ddd5a8b7878ec7954c950a9193
Rene Martin Loperena Garcia <rene@pinwheelapi.com>
2023-12-19 15:06:35 -0800

    Update SDK events

--- android/src/main/java/com/underdog_tech/react/PinwheelViewManager.kt
+++ android/src/main/java/com/underdog_tech/react/PinwheelViewManager.kt
@@ -162,7 +162,7 @@ class PinwheelViewManager(
 
 
     this.token?.let {
-      val pinwheelFragment = PinwheelFragment.newInstance(it, "react native", "2.5.1")
+      val pinwheelFragment = PinwheelFragment.newInstance(it, "react native", "3.0.0")
       pinwheelFragment.pinwheelEventListener = this
       val activity = reactContext.currentActivity as FragmentActivity
       activity.supportFragmentManager
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // @ts-ignore-next-line
 import Pinwheel from './pinwheel-wrapper';
 import {SafeAreaView, StyleSheet} from 'react-native';
-import { VERSION } from './constants';
+import { LinkOptions } from './client-events/client';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,93 +10,9 @@ const styles = StyleSheet.create({
   },
 });
 
-export const PINWHEEL_MESSAGE_TYPES = {
-  PINWHEEL_EXIT: 'PINWHEEL_EXIT',
-  PINWHEEL_MODAL_CLOSE: 'PINWHEEL_MODAL_CLOSE',
-  PINWHEEL_LOAD_COMPLETE: 'PINWHEEL_LOAD_COMPLETE',
-  PINWHEEL_SUCCESS: 'PINWHEEL_SUCCESS',
-  PINWHEEL_EVENT: 'PINWHEEL_EVENT',
-};
-
-export type LinkResult = {
-  accountId: string;
-  platformId: string;
-  job: string;
-  params: {
-    amount?: { value: number; unit: '%' | '$' };
-  };
-};
-
-export type ScreenTransition = {
-  screenName: string;
-  selectedEmployerId?: string;
-  selectedEmployerName?: string;
-  selectedPlatformId?: string;
-  selectedPlatformName?: string;
-}
-
-/**
- * @deprecated This type will be removed in version 2.4. Use the renamed type `PinwheelErrorType`
- * instead.
- */
-export type ErrorType =
-  | 'clientError'
-  | 'systemError'
-  | 'userActionRequired'
-  | 'platformError'
-  | 'invalidAccountsConfiguration'
-  | 'invalidUserInput'
-  | 'invalidLinkToken';
-
-export type PinwheelErrorType = ErrorType
-
-/**
- * @deprecated The type should not be used as it clashes with the native JS `Error` object.
- * You should use `PinwheelError` instead. `Error` will be removed in version 2.4
- */
-export type Error = {
-  type: PinwheelErrorType;
-  code: string;
-  message: string;
-  pendingRetry: boolean;
-};
-
-// Export `Error` as `PinwheelError` to avoid native `Error` namespace clash
-export type PinwheelError = Error
-
-export type EmptyPayloadObject = Record<string, never>
-
-export type EventPayload =
-  | { selectedEmployerId: string; selectedEmployerName: string }
-  | { selectedPlatformId: string; selectedPlatformName: string }
-  | { value: number; unit: '%' | '$' }
-  | LinkResult
-  | { accountId: string; platformId: string }
-  | ScreenTransition
-  | PinwheelError
-  | EmptyPayloadObject
-  | undefined
-
-type PinwheelProps = {
-  linkToken: string,
-  onLogin?: (result: { accountId: string; platformId: string }) => void;
-  onLoginAttempt?: (result: { platformId: string }) => void;
-  onSuccess?: (result: LinkResult) => void;
-  onError?: (error: PinwheelError) => void;
-  onExit?: (error: PinwheelError | EmptyPayloadObject) => void;
-  onEvent?: (eventName: string, payload: EventPayload) => void;
-}
-
-type NativeEvent = {
-  data: string
-}
-
-type WebViewEvent = {
-  nativeEvent: NativeEvent 
-}
+type PinwheelProps = {} & LinkOptions
 
 export default ({linkToken, onLogin, onLoginAttempt, onSuccess, onError, onExit, onEvent}: PinwheelProps) => {
-
   const handleEvent = (event: any) => {
     if (!event) {
       // first event is always an empty string
@@ -104,7 +20,6 @@ export default ({linkToken, onLogin, onLoginAttempt, onSuccess, onError, onExit,
     }
 
     const { name, payload } = event;
-
     onEvent && onEvent(name, payload);
 
     switch (name.toLowerCase()) {
