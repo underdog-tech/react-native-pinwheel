6eb01c466b590c4cfb65418578c15f33af107218
Rene Martin Loperena Garcia <rene@pinwheelapi.com>
2023-12-20 09:54:03 -0800

    Add client events

---
+++ src/client-events/registry/v3.ts
@@ -0,0 +1,47 @@
+/* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/ban-types */
+import type { AdditionsType, ModificationsType, RemovalsType } from '../utils'
+import type { EventPayloadMap as EventPayloadMapV2, LinkApiJob } from './v2.3'
+
+import { InputAllocationEventPayload as InputAllocationEventPayload2_3 } from './v2.3'
+
+/*
+ * Export all the types from V3 and we override whatever changes
+ */
+export * from './v2.3'
+
+export type SuccessEventPayload = {
+  accountId: string
+  platformId: string
+  job: LinkApiJob
+  params?: InputAllocationEventPayload
+}
+
+export type InputAllocationEventPayload =
+  | { action: null; allocation: null }
+  | InputAllocationEventPayload2_3
+
+/**
+ * @deprecated - Use `InputAllocationEventPayload` instead.
+ */
+export type InputAllocation = InputAllocationEventPayload
+
+type EventPayloadAdditions = {}
+
+type EventPayloadModifications = {
+  input_allocation: InputAllocationEventPayload
+}
+
+type EventPayloadRemovals = ['input_amount']
+
+export type EventPayloadMap = Omit<
+  EventPayloadMapV2 &
+    AdditionsType<EventPayloadAdditions> &
+    ModificationsType<EventPayloadModifications>,
+  keyof RemovalsType<EventPayloadRemovals>
+>
+
+export type EventHandler = <T extends keyof EventPayloadMap>(
+  eventName: T,
+  payload: EventPayloadMap[T]
+) => void
