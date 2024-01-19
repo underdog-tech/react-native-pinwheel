470612f19da10be7099cab4368cc603f0438125a
Robby Abaya <rabaya@masterworks.io>
2024-01-19 10:19:15 -0500

    V3.0.1 (#104)
    * v3.0.1
    
    * Fix changelog
    
    * update constants file
    
    * update version in example
    
    * Update changelog
    
    ---------
    
    Co-authored-by: roscioli <octavio@getpinwheel.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,4 @@
 /**
  * Importing package.json here causes a problem with the folder structure when we npm pack and publish.
  */
-export const VERSION = '3.0.0';
-
-export const LINK_PAGE_URL = 'https://cdn.getpinwheel.com/link-v2.3.0.html';
-
-export const PINWHEEL_DOMAIN = 'getpinwheel.com';
+export const VERSION = '3.0.1';
