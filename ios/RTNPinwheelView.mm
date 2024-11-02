9883359ae2d5b878fabaa8076cb30c7f7b2ba8ab
Allen Gao <127158095+allen-pinwheel@users.noreply.github.com>
2024-11-01 17:56:16 -0700

    V3.2.0-rc.1 (#122)

--- ios/RTNPinwheelView.mm
+++ ios/RTNPinwheelView.mm
@@ -35,8 +35,8 @@ using namespace facebook::react;
 }
 
 - (void)initPinwheelWrapperVC {
-    if (self.token != nil && self.pinwheelWrapperVC == nil) {    
-        self.pinwheelWrapperVC = [[PinwheelVCWrapper alloc] initWithToken:self.token delegate:self sdk:@"react native" version: @"3.0.5"];
+    if (self.token != nil && self.pinwheelWrapperVC == nil) {
+        self.pinwheelWrapperVC = [[PinwheelVCWrapper alloc] initWithToken:self.token delegate:self sdk:@"react native" version: @"3.2.0-rc.1"];
         [self addSubview:self.pinwheelWrapperVC.view];
     }
 }
